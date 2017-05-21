6af133f2f99c662d66770f1a5b4486a4e34ef43a
pha4 <pha4@csc.com>
2017-05-21 22:53:24 +0700

    update codes

---
+++ Create dbo.ItemType.sql
@@ -0,0 +1,32 @@
+﻿USE [aspnet-vnexchange1-92bcff6c-24d8-4a56-8f12-957817bb64cd]
+GO
+
+/****** Object: Table [dbo].[ItemType] Script Date: 5/19/2017 10:54:22 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ItemType] (
+    [ItemTypeId]          INT            IDENTITY (1, 1) NOT NULL,
+    [ItemId]              INT            NULL,
+    [ItemTypeDescription] NVARCHAR (MAX) NULL
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ItemType_ItemId]
+    ON [dbo].[ItemType]([ItemId] ASC);
+
+
+GO
+ALTER TABLE [dbo].[ItemType]
+    ADD CONSTRAINT [PK_ItemType] PRIMARY KEY CLUSTERED ([ItemTypeId] ASC);
+
+
+GO
+ALTER TABLE [dbo].[ItemType]
+    ADD CONSTRAINT [FK_ItemType_Item_ItemId] FOREIGN KEY ([ItemId]) REFERENCES [dbo].[Item] ([ItemId]);
+
+
--- vnexchange1/Startup.cs
+++ vnexchange1/Startup.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using React.AspNet;
 using vnexchange1.Data;
 using vnexchange1.Models;
 using vnexchange1.Services;
+
 
 namespace vnexchange1
 {
@@ -32,6 +31,7 @@ namespace vnexchange1
 
             builder.AddEnvironmentVariables();
             Configuration = builder.Build();
+
         }
 
         public IConfigurationRoot Configuration { get; }
@@ -47,11 +47,20 @@ namespace vnexchange1
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            //services.AddDbContext<VnExchangeContext>(options => 
+            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc();
 
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
+
+            // Add framework services.
+            services.AddMvc();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -83,6 +92,30 @@ namespace vnexchange1
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseStaticFiles();
+
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                //  .AddScript("~/Scripts/First.jsx")
+                //  .AddScript("~/Scripts/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+            });
+
+            //VnExchangeInitializer.Initialize(context);
         }
     }
 }
