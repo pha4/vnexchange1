861fdf10b1a862e16a2cb6fd6d6aee7c648fafe9
is24exam <stefan.landgraf@scout24.ch>
2017-08-15 10:00:10 +0700

    test

--- vnexchange1/Models/AccountViewModels/RegisterViewModel.cs
+++ vnexchange1/Models/AccountViewModels/RegisterViewModel.cs
@@ -14,17 +14,17 @@ namespace vnexchange1.Models.AccountViewModels
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "{0} phải tối thiểu {2} và tối đa {1} kí tự.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Mật khẩu")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Xác nhận mật khẩu")]
+        [Compare("Password", ErrorMessage = "Mật khẩu và mật khẩu xác nhận không trùng khớp nhau")]
         public string ConfirmPassword { get; set; }
 
-        [Display(Name = "User name")]
+        [Display(Name = "Tên tài khoản")]
         public string UserName { get; set; }
     }
 }
